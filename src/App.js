98d05a92682bc95263fc43c40315f466811f9061
RubinderG <82204427+RubinderG@users.noreply.github.com>
2022-02-10 15:07:55 +0000

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,20 +4,22 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather Application </h1>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://www.linkedin.com/in/rubinder/" target="blank">
-          Rubinder {""}
-        </a>
-        and is {""}
-        <a
-          href="https://github.com/RubinderG/react-weatherapplication"
-          target="blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather Application </h1>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://www.linkedin.com/in/rubinder/" target="blank">
+            Rubinder {""}
+          </a>
+          and is {""}
+          <a
+            href="https://github.com/RubinderG/react-weatherapplication"
+            target="blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
