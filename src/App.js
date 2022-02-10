9caba43d5f23f10f2bc381eef3c577de3385d4dc
RubinderG <82204427+RubinderG@users.noreply.github.com>
2022-02-10 11:51:10 +0000

    Added Footer

--- src/App.js
+++ src/App.js
@@ -2,5 +2,18 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather Application</div>;
+  return (
+    <div className="App">
+      Weather Application
+      <footer>
+        This project was coded by Rubinder and is {""}
+        <a
+          href="https://github.com/RubinderG/react-weatherapplication"
+          target="blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
