c146e7ffaae373ce2efc4ce4b005266a1fd3c48e
RubinderG <82204427+RubinderG@users.noreply.github.com>
2022-02-11 18:52:54 +0000

    Aligning CSS

--- src/Weather.js
+++ src/Weather.js
@@ -30,14 +30,16 @@ export default function Weather() {
         <li>Thursday 11:11</li>
         <li>Clear Skys </li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <img
-            src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="clear skys"
-          />
-          <span className="temperature">6</span>{" "}
-          <span className="unit">°C</span>
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="clear skys"
+            />
+            <span className="temperature">6</span>{" "}
+            <span className="unit">°C</span>
+          </div>
         </div>
         <div className="col-6">
           <ul>
