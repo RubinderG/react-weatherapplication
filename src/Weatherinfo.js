87fb3ea74cc269efb2fae509566b36c7148358a3
RubinderG <82204427+RubinderG@users.noreply.github.com>
2022-02-12 13:59:48 +0000

    Importing an animated weather icon api

--- src/Weatherinfo.js
+++ src/Weatherinfo.js
@@ -1,5 +1,6 @@
 import React from "react";
 import FormattedDate from "./FormattedDate";
+import WeatherIcon from "./WeatherIcon";
 
 export default function WeatherInfo(props) {
   return (
@@ -15,7 +16,8 @@ export default function WeatherInfo(props) {
         <div className="col-6">
           <div className="clearfix">
             <div className="float-left">
-              
+              <WeatherIcon code={props.data.icon}
+              />
             </div>
 
             <div className="float-left">
